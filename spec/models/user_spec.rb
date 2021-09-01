84668faf0eada478e10edb78949b16750dc0afde
CKyang <pithend58639@gmail.com>
2021-09-02 03:31:46 +0800

    add column & user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'name must be present' do
+      it 'is not valid without a name' do
+        user = User.new(phone: '123', email: 'test@ck.com')
+        expect(user).not_to be_valid
+      end
+
+      it 'is valid with a name' do
+        user = User.new(name: 'ck', phone: '123', email: 'test@ck.com')
+        expect(user).to be_valid
+        expect(user.name).to eq('ck')
+      end
+    end
+  end
 end
